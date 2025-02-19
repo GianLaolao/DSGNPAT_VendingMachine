c9136b2c2b03af0d5927daf3d6c0c976e44cf255
Vladimir <vladimirtang@gmail.com>
2025-02-19 17:29:48 +0800

    Simplified code for money change and magic number of for total sales

--- src/MoneyCalc.java
+++ src/MoneyCalc.java
@@ -45,131 +45,29 @@ public class MoneyCalc {
             return null;
         }
 
+        // adding cash to the money box
         for (int i = 0; i < 9; i++) {
             vendoMoney.setDenom(i, vendoMoney.getCash(i).getQuantity() + userMoney.getDenominations()[i].getQuantity());
         }
 
-        if (vendoMoney.getCash(8).getQuantity() != 0){
-            int amount = change / 1000;
-            if ((change / 1000 != 0) && amount > vendoMoney.getCash(8).getQuantity()) {
-                change -= vendoMoney.getCash(8).getQuantity() * 100;
-                changeBox.setDenom(8, vendoMoney.getCash(8).getQuantity());
-                vendoMoney.setDenom(8, 0);
-            }
-            else if (change / 1000 != 0) {
-                change -= amount * 1000;
-                vendoMoney.setDenom(8, vendoMoney.getCash(8).getQuantity() - amount);
-                changeBox.setDenom(8, amount);
-            }
-        }
-        if (vendoMoney.getCash(7).getQuantity() != 0){
-            int amount = change / 500;
-            if ((change / 500 != 0) && amount > vendoMoney.getCash(7).getQuantity()) {
-                change -= vendoMoney.getCash(7).getQuantity() * 50;
-                changeBox.setDenom(7,vendoMoney.getCash(7).getQuantity());
-                vendoMoney.setDenom(7,0);
-            }
-            else if (change /500 != 0) {
-                change -= amount * 500;
-                vendoMoney.setDenom(7, vendoMoney.getCash(7).getQuantity() - amount);
-                changeBox.setDenom(7,amount);
+        int[] cashDenominations = vendoMoney.getCashDenominations();
+        for (int i = 8; i >= 0; i-- )
+        {
+            int currentDenomination = cashDenominations[i];
+            if (vendoMoney.getCash(i).getQuantity() != 0){
+                int amount = change / currentDenomination;
+                if ((change / currentDenomination != 0) && amount > vendoMoney.getCash(i).getQuantity()) {
+                    change -= vendoMoney.getCash(i).getQuantity() * 100;
+                    changeBox.setDenom(i, vendoMoney.getCash(i).getQuantity());
+                    vendoMoney.setDenom(i, 0);
+                }
+                else if (change / currentDenomination != 0) {
+                    change -= amount * currentDenomination;
+                    vendoMoney.setDenom(i, vendoMoney.getCash(i).getQuantity() - amount);
+                    changeBox.setDenom(i, amount);
+                }
             }
         }
-
-         if (vendoMoney.getCash(6).getQuantity() != 0){
-            int amount = change / 200;
-            if ((change / 200 != 0) && amount > vendoMoney.getCash(6).getQuantity()) {
-                change -= vendoMoney.getCash(6).getQuantity() * 50;
-                changeBox.setDenom(6,vendoMoney.getCash(6).getQuantity());
-                vendoMoney.setDenom(6,0);
-            }
-            else if (change /200 != 0) {
-                change -= amount * 200;
-                vendoMoney.setDenom(6, vendoMoney.getCash(6).getQuantity() - amount);
-                changeBox.setDenom(6,amount);
-            }
-        }
-         if (vendoMoney.getCash(5).getQuantity() != 0){
-            int amount = change / 100;
-            if ((change / 100 != 0) && amount > vendoMoney.getCash(5).getQuantity()) {
-                change -= vendoMoney.getCash(5).getQuantity() * 100;
-                changeBox.setDenom(5,vendoMoney.getCash(5).getQuantity());
-                vendoMoney.setDenom(5, 0);
-            }
-            else if (change / 100 != 0) {
-                change -= amount * 100;
-                vendoMoney.setDenom(5, vendoMoney.getCash(5).getQuantity() - amount);
-                changeBox.setDenom(5, amount);
-            }
-        }
-
-        if (vendoMoney.getCash(4).getQuantity() != 0){
-            int amount = change / 50;
-            if ((change / 50 != 0) && amount > vendoMoney.getCash(4).getQuantity()) {
-                change -= vendoMoney.getCash(4).getQuantity() * 50;
-                changeBox.setDenom(4, vendoMoney.getCash(4).getQuantity());
-                vendoMoney.setDenom(4, 0);
-            }
-            else if (change /50 != 0) {
-                change -= amount * 50;
-                vendoMoney.setDenom(4, vendoMoney.getCash(4).getQuantity() - amount);
-                changeBox.setDenom(4, amount);
-            }
-        }
-        if (vendoMoney.getCash(3).getQuantity() != 0){
-            int amount = change / 20;
-            if ((change / 20 != 0) && amount > vendoMoney.getCash(3).getQuantity()) {
-                change -= vendoMoney.getCash(3).getQuantity() * 20;
-                changeBox.setDenom(3, vendoMoney.getCash(3).getQuantity());
-                vendoMoney.setDenom(3, 0);
-            }
-            else if (change / 20 != 0){
-                change -= amount * 20;
-                vendoMoney.setDenom(3, vendoMoney.getCash(3).getQuantity() - amount);
-                changeBox.setDenom(3, amount);
-            }
-        }
-        if (vendoMoney.getCash(2).getQuantity() != 0){
-            int amount = change / 10;
-            if ((change / 10 != 0) && amount > vendoMoney.getCash(2).getQuantity()) {
-                change -= vendoMoney.getCash(2).getQuantity() * 10;
-                changeBox.setDenom(2, vendoMoney.getCash(2).getQuantity());
-                vendoMoney.setDenom(2, 0);
-            }
-            else if (change / 10 != 0){
-                change -= amount * 10;
-                vendoMoney.setDenom(2, vendoMoney.getCash(2).getQuantity() - amount);
-                changeBox.setDenom(2, amount);
-            }
-        }
-        if (vendoMoney.getCash(1).getQuantity() != 0){
-            int amount = change / 5;
-            if ((change / 5 != 0) && amount > vendoMoney.getCash(1).getQuantity()) {
-                change -= vendoMoney.getCash(1).getQuantity() * 5;
-                changeBox.setDenom(1, vendoMoney.getCash(1).getQuantity());
-                vendoMoney.setDenom(1,0);
-            }
-            else if (change / 5 != 0){
-                change -= amount * 5;
-                vendoMoney.setDenom(1, vendoMoney.getCash(1).getQuantity() - amount);
-                changeBox.setDenom(1, amount);
-            }
-
-        }
-        if (vendoMoney.getCash(0).getQuantity() != 0){
-            int amount = change / 1;
-            if ((change / 1 != 0) && amount > vendoMoney.getCash(0).getQuantity()) {
-                change -= vendoMoney.getCash(0).getQuantity();
-                vendoMoney.setDenom(0, 0);
-                changeBox.setDenom(0, amount);
-            }
-            else if (change / 1 != 0){
-                change -= amount;
-                vendoMoney.setDenom(0, vendoMoney.getCash(0).getQuantity() - amount);
-                changeBox.setDenom(0, amount);
-            }
-        }
-
 
         if (change != 0)
             return null;
